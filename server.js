7b352d0dc1aa670135563be53f51be5fada734ef
BenjaminS <bsabardu@gmail.com>
2021-03-17 11:13:14 +0100

    test without server

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ const privateKey = fs.readFileSync('/etc/letsencrypt/live/bensab.fr/privkey.pem'
 const certificate = fs.readFileSync('/etc/letsencrypt/live/bensab.fr/cert.pem', 'utf8');
 const ca = fs.readFileSync('/etc/letsencrypt/live/bensab.fr/chain.pem', 'utf8');
 
+// Handle https redirect
+app.use((req, res, next) => {
+  if (req.secure) {
+    // request was via https, so do no special handling
+    next();
+  }
+  else {
+    // request was via http, so redirect to https
+    res.redirect(`https://${req.headers.host}${req.url}`);
+  }
+});
+
 // add middlewares
 app.use(express.static(path.join(__dirname, 'build'), { dotfiles: 'allow' }));
 
@@ -20,17 +32,13 @@ const credentials = {
   ca: ca,
 };
 
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 // Starting both http & https servers
-const httpServer = http.createServer(app);
+// const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(3000, () => {
-  console.log('HTTP Server running on port 3000');
-});
+// httpServer.listen(80, () => {
+//   console.log('HTTP Server running on port 80');
+// });
 
 httpsServer.listen(443, () => {
   console.log('HTTPS Server running on port 443');
