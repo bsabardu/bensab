7e3bb23054caeedc5fe3864f31204dc8653950f7
BenjaminS <bsabardu@gmail.com>
2021-03-17 10:19:26 +0100

    add https server

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const port = 4000;
 
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/bensab.fr/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/bensab.fr/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+
 // add middlewares
 app.use(express.static(path.join(__dirname, 'build'), { dotfiles: 'allow' }));
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
 
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
 });
