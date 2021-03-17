2b1f79854b3bbbc1250d6cf1cfafea8485a51bb3
BenjaminS <bsabardu@gmail.com>
2021-03-17 10:02:56 +0100

    change build directory

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const app = express();
 const port = 4000;
 
 // add middlewares
-app.use(express.static(path.join(__dirname, 'dist')));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'build'), { dotfiles: 'allow' }));
 
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
